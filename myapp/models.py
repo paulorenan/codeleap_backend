1a139020d08d70e2fa3331ae1167fda718e4a0c5
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2024-02-22 22:37:15 -0300

    add unique to username

--- myapp/models.py
+++ myapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Career(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
     created_datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
